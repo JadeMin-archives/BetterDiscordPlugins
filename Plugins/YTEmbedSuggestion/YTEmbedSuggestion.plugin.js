728946252981fb442a02bdb4725471aa40c5d8d7
JadeMin <36400787+JadeMin@users.noreply.github.com>
2021-08-15 07:37:55 +0900

    Update YTEmbedSuggestion.plugin.js

--- Plugins/YTEmbedSuggestion/YTEmbedSuggestion.plugin.js
+++ Plugins/YTEmbedSuggestion/YTEmbedSuggestion.plugin.js
@@ -3,34 +3,35 @@
  * @version 0.1.0
  * @author KlartNET
  * @authorId 840594543291269120
- * @description Replaces irrelevant video recommendations that appear when playing a YouTube embedded video on Discord. (You can't remove "More Videos" itself with this plugin.)
- * @source https://github.com/JadeMin/BetterDiscordAddons/
+ * @description Replaces irrelevant video recommendations that appear when playing a YouTube embedded video on Discord. _**(You can't remove "More Videos" itself with this plugin.)**_
+ * @source https://github.com/JadeMin/BetterDiscordAddons/tree/main/Plugins/YTEmbedSuggestion/
  * @updateUrl https://raw.githubusercontent.com/JadeMin/BetterDiscordAddons/main/Plugins/YTEmbedSuggestion/YTEmbedSuggestion.plugin.js
  **/
 
 
 module.exports = (()=> {
 	const config = {
-		"info": {
+		info: {
 			"name": "YTEmbedSuggestion",
 			"author": "KlartNET",
 			"version": "0.1.0",
-			"description": "Replaces irrelevant video recommendations that appear when playing a YouTube embedded video on Discord. (You can't remove \"More Videos\" itself with this plugin.)"
+			"description": "Replaces irrelevant video recommendations that appear when playing a YouTube embedded video on Discord. _**(You can't remove \"More Videos\" itself with this plugin.)**_"
 		}
 	};
 
 
 	return class {
+		constructor(){
+			this._config = config;
+			this._interval = null;
+		}
 		getName(){ return config.info.name; }
 		getAuthor(){ return config.info.author; }
 		getVersion(){ return config.info.version; }
 		getDescription(){ return config.info.description; }
-
-		load() {
-			this.interval = null;
-		}
+		
 		start() {
-			this.interval = setInterval(()=> {
+			this._interval = setInterval(()=> {
 				document.querySelectorAll("iframe[src^='https://www.youtube.com/embed/']").forEach(element=> {
 					const identifier = element.src.endsWith("&rel=0");
 					if(!identifier) {
@@ -45,10 +46,10 @@ module.exports = (()=> {
 			}, 0);
 		} 
 		stop() {
-			if(this.interval) {
-				clearInterval(this.interval);
+			if(this._interval) {
+				clearInterval(this._interval);
 			} else {
-				console.warn(`[${config.info.name}] interval is ${interval}`);
+				console.warn(`[${config.info.name}] interval is ${this._interval}`);
 			}
 		}
 	};
