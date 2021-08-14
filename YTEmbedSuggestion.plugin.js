246cff8deda8107d5dadeee39ac3a000f21d03d1
JadeMin <36400787+JadeMin@users.noreply.github.com>
2021-08-15 02:12:28 +0900

    Update YTEmbedSuggestion.plugin.js

--- YTEmbedSuggestion.plugin.js
+++ YTEmbedSuggestion.plugin.js
@@ -9,24 +9,44 @@
  **/
 
 
-class YTEmbedSuggestion {
-	interval = 
-	start() {
-		interval = setInterval(()=> {
-			document.querySelectorAll("iframe[src^='https://www.youtube.com/embed/']").forEach(element=> {
-				const identifier = element.src.endsWith("&rel=0");
-				if(!identifier) {
-					console.log({
-						src: element.src,
-						identifier
-					});
-				}	
+module.exports = (()=> {
+	const config = {
+		"info": {
+			"name": "YTEmbedSuggestion",
+			"author": "KlartNET",
+			"version": "0.1.0",
+			"authorId": "840594543291269120",
+			"description": "Allows you to download all Themes from BD's Website within Discord",
+			"source": "https://github.com/JadeMin/YTEmbedSuggestion",
+			"updateUrl": "https://raw.githubusercontent.com/JadeMin/YTEmbedSuggestion/main/YTEmbedSuggestion.plugin.js",
+		}
+	};
 
-				if(!identifier) element.src += "&rel=0";
-			});
-		}, 0);
-	}
-	stop() {
-		clearInterval(interval);
-	}
-};
+
+	return class {
+		getName () {return config.info.name;}
+		getAuthor () {return config.info.author;}
+		getVersion () {return config.info.version;}
+		start() {
+			this.interval = setInterval(()=> {
+				document.querySelectorAll("iframe[src^='https://www.youtube.com/embed/']").forEach(element=> {
+					const identifier = element.src.endsWith("&rel=0");
+					if(!identifier) {
+						console.log({
+							src: element.src,
+							identifier
+						});
+					}	
+
+					if(!identifier) element.src += "&rel=0";
+				});
+			}, 0);
+		} 
+		stop() {
+			if(interval) clearInterval(this.interval);
+			else {
+				console.warn(`[${config.info.name}] interval is ${interval}`);
+			}
+		}
+	};
+})();
