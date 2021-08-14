6142d9b48b60d0e61c8af8a114f7776902dd38a8
JadeMin <36400787+JadeMin@users.noreply.github.com>
2021-08-15 01:46:43 +0900

    Create YTEmbedSuggestion.js

---
+++ YTEmbedSuggestion.js
@@ -0,0 +1,31 @@
+/**
+ * @name YTEmbedSuggestions
+ * @version 0.1.0 Beta
+ * @author KlartNET
+ * @description Removes the suggestion of random unrelated videos that appear when you pause any YouTube embedded video on Discord.
+ */
+
+
+let interval = null;
+
+class YTEmbedSuggestions {
+	start() {
+		interval = setInterval(()=> {
+			document.querySelectorAll("iframe[src^='https://www.youtube.com/embed/']").forEach(element=> {
+				const identifier = element.src.endsWith("&rel=0");
+				if(!identifier) {
+					console.log({
+						src: element.src,
+						identifier
+					});
+				}
+
+
+				if(!identifier) element.src += "&rel=0";
+			});
+		}, 0);
+	}
+	stop() {
+		clearInterval(interval);
+	}
+};
