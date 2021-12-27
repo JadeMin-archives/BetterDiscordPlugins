0952b61b261dc7e01c3330ba191a21a852c59599
JadeMin <36400787+JadeMin@users.noreply.github.com>
2021-12-27 16:54:21 +0900

    Update NitroBypass.plugin.js

--- NitroBypass/NitroBypass.plugin.js
+++ NitroBypass/NitroBypass.plugin.js
@@ -41,7 +41,7 @@ module.exports = (()=> {
 				discord_id: "000000000000000000",*/
 				github_username: "JadeMin"
 			}],
-			version: "1.0.40012",
+			version: "1.0.40013",
 			description: "고해상도의 방송 송출을 니트로 없이 사용하세요!",
 			github: "https://github.com/JadeMin/BetterDiscordPlugins/",
 			github_raw: "https://raw.githubusercontent.com/JadeMin/BetterDiscordPlugins/main/NitroBypass/NitroBypass.plugin.js"
@@ -183,7 +183,7 @@ module.exports = (()=> {
 						gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${properties.gtag.id}`;
 						gtagScript.className = properties.gtag.className;
 
-						script.class = properties.gtag.className;
+						script.className = properties.gtag.className;
 						script.innerHTML = `
 							window.dataLayer = window.dataLayer || [];
 							function gtag(){ dataLayer.push(arguments); }
