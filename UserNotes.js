714048f0c3e9388dc47e32606a3be6675472c5cb
KlartNET <dev1234@klartnet.ml>
2022-01-22 04:23:47 +0900

    Create UserNotes.js

---
+++ UserNotes.js
@@ -0,0 +1,30 @@
+class NOTEY {
+	constructor(){
+		this._NOTES = null;
+	}
+
+	fetchNotes(){
+		return new Promise(resolve => {
+			ZLibrary.DiscordModules.APIModule.get(ZLibrary.DiscordModules.DiscordConstants.Endpoints.NOTES).then(response=> {
+				this._NOTES = [response.body, response];
+				resolve([response.body, response]);
+			});
+		});
+	}
+	updateNote(id, value){
+		return ZLibrary.WebpackModules.getByProps("updateNote").updateNote(id, value);
+	}
+};
+const Notey = new NOTEY();
+
+
+// fetches all users' note data from Discord API (Endpoints)
+Notey.fetchNotes().then(([userNotes, _ApiResponse])=> {
+	for(const id in userNotes) {
+		const targetUser = ZLibrary.DiscordModules.UserStore.getUser(id);
+		console.log(`${targetUser || "Unknown"} (${id}) | ${userNotes[id]}`);
+	}
+});
+
+// edits a specific user's note
+Notey.updateNote('id', "a note content to be edited");
