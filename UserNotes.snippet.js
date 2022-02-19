236fdf124eb9a6c0939a765a348ab0f12c62c34e
KlartNET <dev1234@klartnet.ml>
2022-02-19 22:24:31 +0900

    Simulates in-plugin

--- UserNotes.snippet.js
+++ UserNotes.snippet.js
@@ -1,3 +1,8 @@
+// Simulates in-plugin
+const { DiscordModules, WebpackModules } = ZLibrary;
+
+
+
 class NOTEY {
 	constructor(){
 		this._NOTES = null;
@@ -5,14 +10,14 @@ class NOTEY {
 
 	fetchNotes(){
 		return new Promise(resolve => {
-			ZLibrary.DiscordModules.APIModule.get(ZLibrary.DiscordModules.DiscordConstants.Endpoints.NOTES).then(response=> {
+			DiscordModules.APIModule.get(DiscordModules.DiscordConstants.Endpoints.NOTES).then(response=> {
 				resolve([response.body, response]);
 				this._NOTES = [response.body, response];
 			});
 		});
 	}
 	updateNote(id, value){
-		return ZLibrary.WebpackModules.getByProps("updateNote").updateNote(id, value);
+		return WebpackModules.getByProps("updateNote").updateNote(id, value);
 	}
 };
 const Notey = new NOTEY();
@@ -21,7 +26,7 @@ const Notey = new NOTEY();
 // fetches all users' note data from Discord API (Endpoints)
 Notey.fetchNotes().then(([userNotes, _ApiResponse])=> {
 	for(const id in userNotes) {
-		const targetUser = ZLibrary.DiscordModules.UserStore.getUser(id);
+		const targetUser = DiscordModules.UserStore.getUser(id);
 		console.log(`${targetUser || "Unknown"} (${id}) | ${userNotes[id]}`);
 	}
 });
